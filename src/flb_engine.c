fluent/fluent-bit

engine: send backlog chunks on shutdown
Signed-off-by: Anuj Singh <singholt@amazon.com>
Co-authored-by: Wesley Pettit <wppttt@amazon.com>

src/flb_engine.c
@@ -964,7 +964,8 @@ int flb_engine_start(struct flb_config *config)
 
     ret = sb_segregate_chunks(config);
 
-    if (ret) {
+    if (ret < 0)
+    {
         flb_error("[engine] could not segregate backlog chunks");
         return -2;
     }
@@ -1049,6 +1050,18 @@ int flb_engine_start(struct flb_config *config)
                     if (ret > 0 && (config->grace_count < config->grace || config->grace == -1)) {
                         if (config->grace_count == 1) {
                             flb_task_running_print(config);
+                            /*                                                                               
+                            * If storage.backlog.shutdown_flush is enabled, attempt to flush pending
+                            * filesystem chunks during shutdown. This is particularly useful in scenarios   
+                            * where Fluent Bit cannot restart to ensure buffered data is not lost.                                             
+                            */
+                            if (config->storage_bl_shutdown_flush) {
+                                ret = sb_segregate_chunks(config);
+                                if (ret < 0) {
+                                    flb_error("[engine] could not segregate backlog chunks during shutdown");
+                                    return -2;
+                                }
+                            }
                         }
                         if ((mem_chunks + fs_chunks) > 0) {
                             flb_info("[engine] pending chunk count: memory=%d, filesystem=%d; grace_timer=%d",

rghouzra/fluent-bit_oracle

src/flb_engine.c
@@ -976,7 +976,8 @@ int flb_engine_start(struct flb_config *config)
 
     ret = sb_segregate_chunks(config);
 
-    if (ret) {
+    if (ret < 0)
+    {
         flb_error("[engine] could not segregate backlog chunks");
         return -2;
     }
@@ -1061,6 +1062,18 @@ int flb_engine_start(struct flb_config *config)
                     if (ret > 0 && (config->grace_count < config->grace || config->grace == -1)) {
                         if (config->grace_count == 1) {
                             flb_task_running_print(config);
+                            /*                                                                               
+                            * If storage.backlog.shutdown_flush is enabled, attempt to flush pending
+                            * filesystem chunks during shutdown. This is particularly useful in scenarios   
+                            * where Fluent Bit cannot restart to ensure buffered data is not lost.                                             
+                            */
+                            if (config->storage_bl_flush_on_shutdown) {
+                                ret = sb_segregate_chunks(config);
+                                if (ret < 0) {
+                                    flb_error("[engine] could not segregate backlog chunks during shutdown");
+                                    return -2;
+                                }
+                            }
                         }
                         if ((mem_chunks + fs_chunks) > 0) {
                             flb_info("[engine] pending chunk count: memory=%d, filesystem=%d; grace_timer=%d",